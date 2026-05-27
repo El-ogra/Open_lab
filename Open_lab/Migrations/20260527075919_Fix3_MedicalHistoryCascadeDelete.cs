f156838c648ec1cb690298b36db8aec3cc806978
medowemado <medowemado@gmail.com>
2026-05-27 11:16:04 +0300

    Fix3: MedicalHistory cascade delete migration

---
+++ Open_lab/Migrations/20260527075919_Fix3_MedicalHistoryCascadeDelete.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Open_lab.Migrations
+{
+    /// <inheritdoc />
+    public partial class Fix3_MedicalHistoryCascadeDelete : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
